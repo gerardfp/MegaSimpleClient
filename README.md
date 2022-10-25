37bc4defb5fa73bb44981ad23d7b2e6ed217fb76
gerardfp <gerardfp@gmail.com>
2022-10-25 14:38:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## install
+
+Add to `build.gradle`
+``` groovy
+repositories {
+    // ...
+    maven { url "https://jitpack.io" }
+}
+
+dependencies {
+    // ...
+    implementation 'com.github.gerardfp:megasimpleclient:1.0'
+}
+```
+
+## use
+```java
+ String response = MegaSimpleClient.fetch("http://localhost:8001");
+```
---
+++ src/main/java/org/example/MegaSimpleClient.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class MegaSimpleClient {
+    public static String fetch(String url) {
+        try {
+            return HttpClient.newHttpClient().send(HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build(), HttpResponse.BodyHandlers.ofString()).body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
